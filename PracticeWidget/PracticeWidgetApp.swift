a5cfbd1855eeaea662ad6bf95947ed047325aebc
Anjin Lee <anjin720jin@naver.com>
2024-10-08 13:38:56 +0900

    first init

---
+++ PracticeWidget/PracticeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeWidgetApp.swift
+//  PracticeWidget
+//
+//  Created by Anjin on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
