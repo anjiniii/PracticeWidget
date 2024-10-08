742c5d7a152e04a5376d0a7ad12668c73af68ae2
Anjin Lee <anjin720jin@naver.com>
2024-10-08 13:42:02 +0900

    create new widget target

---
+++ TargetWidget/TargetWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TargetWidgetBundle.swift
+//  TargetWidget
+//
+//  Created by Anjin on 10/8/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TargetWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TargetWidget()
+    }
+}
